04fda9a6d5cf4ef2178702af91202b69b619a651
nvb-uy <almhak@proton.me>
2023-03-11 06:52:52 -0300

    feat(config optimizations)

--- FABRIC-QUILT/src/main/java/elocindev/item_obliterator/fabric_quilt/mixin/VillagerTradeMixin.java
+++ FABRIC-QUILT/src/main/java/elocindev/item_obliterator/fabric_quilt/mixin/VillagerTradeMixin.java
@@ -5,8 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import elocindev.item_obliterator.fabric_quilt.config.ConfigBuilder;
-import elocindev.item_obliterator.fabric_quilt.config.ConfigEntries;
+import elocindev.item_obliterator.fabric_quilt.ItemObliterator;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.screen.MerchantScreenHandler;
@@ -15,22 +14,19 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.village.TradeOfferList;
 
 @Mixin(MerchantScreenHandler.class)
-public class VillagerTradeMixin {    
-    ConfigEntries Config = ConfigBuilder.loadConfig();
-    
+public class VillagerTradeMixin {
     // Credits to https://github.com/pitbox46/ItemBlacklist
     // wouldn't know how to disable the trades without that mod.
 
     @Inject(at = {@At("RETURN")}, method = {"getRecipes"}, cancellable = true)
     public void getRecipes(CallbackInfoReturnable<TradeOfferList> info) {
         if(info.getReturnValue() != null) {
-            Config = ConfigBuilder.loadConfig();
             
             TradeOfferList Offers = new TradeOfferList(Util.make(new NbtCompound(), 
                 tag -> tag.put("Recipes", new NbtList())));
 
             info.getReturnValue().forEach(offer -> {
-                if(!Config.blacklisted_items.contains(Registry.ITEM.getId(offer.getSellItem().getItem()).toString()))
+                if(!ItemObliterator.Config.blacklisted_items.contains(Registry.ITEM.getId(offer.getSellItem().getItem()).toString()))
                     Offers.add(offer);
             });
             
